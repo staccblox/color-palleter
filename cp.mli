a316910af48caa9bd2fe64a96075ff1c3ee6a195
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-23 19:03:49 -0400

    Update cp.mli

--- cp.mli
+++ cp.mli
@@ -1,1 +1,8 @@
-print_endline "Hello World!"
+- : string colors = [|"red"; "green"; "blue"; "cyan"; "yellow"; "magenta"|]
+- : string palletes = [|"yellow magenta"; "cyan magenta"; "cyan yellow"; "green blue"; ""; ""|]
+for i = 0 to 5 do
+  print_endline colors.(i);;
+  print_endline "goes with"
+  print_endline palletes.(i);;
+  print_endline ""
+done
